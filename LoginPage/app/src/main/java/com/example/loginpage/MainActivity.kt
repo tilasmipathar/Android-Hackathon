45374abb2ac57e22773b9943a6d8aceb874429d8
rachit3006 <arachit3006@gmail.com>
2022-01-07 15:12:05 +0530

    Firebase working

--- LoginPage/app/src/main/java/com/example/loginpage/MainActivity.kt
+++ LoginPage/app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.loginpage
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+const val TAG = "test"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
